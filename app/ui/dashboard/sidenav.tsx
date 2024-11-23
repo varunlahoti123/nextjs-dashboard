131878b081a977ba313ed072426b71c4ba505408
vaunlahoti123 <varunlahoti@datavant.com>
2024-11-22 21:05:23 -0500

    fix to ensure build in vercel

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -22,7 +22,7 @@ export default function SideNav() {
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
-            <SignOutButton onClick={() => console.log('Sign out clicked')}>
+            <SignOutButton >
               <div>Sign Out</div>
             </SignOutButton>
           </button>
